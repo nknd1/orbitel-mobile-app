6698516e4d8ee1aae1a2e8efa74775aa9fdc0bf8
ndenisovich <prodblood@gmail.com>
2024-04-03 19:26:09 +0500

    update ui

--- app/src/main/java/com/example/myorbitel/fragments/TariffAndServiceFragment.kt
+++ app/src/main/java/com/example/myorbitel/fragments/TariffAndServiceFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.example.myorbitel.R
 
@@ -16,6 +17,9 @@ class TariffAndServiceFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/myorbitel/managers/AdaptiveTextView.kt
+++ app/src/main/java/com/example/myorbitel/managers/AdaptiveTextView.kt
@@ -1,4 +1,31 @@
 package com.example.myorbitel.managers
+import android.content.Context
+import android.util.AttributeSet
+import android.util.TypedValue
+import androidx.appcompat.widget.AppCompatTextView
+class AdaptiveTextView : AppCompatTextView {
+    constructor(context: Context) : super(context){
+        initialize()
+    }
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs){
+        initialize()
+    }
 
-class AdaptiveTextView {
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    private fun initialize() {
+        val screenWidth = resources.displayMetrics.widthPixels
+        val screenHeight = resources.displayMetrics.heightPixels
+
+        val textWidthRatio = 0.8
+        val textHeightRatio = 0.5
+
+        val textSize = (screenWidth * textWidthRatio + screenHeight * textHeightRatio) / 2
+
+        setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize.toFloat())
+        val textSizeInSp = 16
+        val density = resources.displayMetrics.density
+        val textSizeInPixels = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, textSizeInSp.toFloat(), resources.displayMetrics)
+
+        gravity = android.view.Gravity.CENTER
+    }
 }
